1b9f521dd8b169e31e6633ca7c809b5cabc6f569
KarenChuang <magic_gua@163.com>
2017-07-12 00:10:05 +0800

    use fetch() and async/await

--- Learn-ES6/es7-async-await.js
+++ Learn-ES6/es7-async-await.js
@@ -1,41 +1,33 @@
-// console.log('--------- es7 async/await ---------');
+import fetch from 'node-fetch'
 
-async function sleep(timeout) {  
-  return new Promise((resolve, reject) => {
-    setTimeout(function() {
-      resolve();
-    }, timeout);
-  });
+// https://api.github.com/users/karenchuang
+
+const USER_NAME = 'karenchuang'
+
+function getProfile() {
+  return fetch(`https://api.github.com/users/${USER_NAME}`)
+}
+
+function getRepos() {
+  return fetch(`https://api.github.com/users/${USER_NAME}/repos`)
+}
+
+async function getCombined() {
+  const profileResponse = await getProfile();
+  const profile = await profileResponse.json();
+
+  const reposResponse = await getRepos();
+  const repos = await reposResponse.json();
+
+  return {
+    profile,
+    repos
+  };
 }
 
 (async function() {
-  console.log('Do some thing, ' + new Date()); // Do some thing, Tue Jul 11 2017 22:01:45 GMT+0800 (CST)
-  await sleep(3000);
-  console.log('Do other things, ' + new Date()); // Do other things, Tue Jul 11 2017 22:01:48 GMT+0800 (CST)
-})();
+  const combined = await getCombined();
+  console.log(combined);
+}());
 
 
-async function funcName(arg) {
-  try {
-    console.log('start', arg);
-    await firstFunction(1000);
-    // waiting...
-    secondaryFunction();
-  } catch (err) {
-    console.log(err);
-  }
-}
-
-function firstFunction(ms) {
-  return new Promise((resolve) => {
-    setTimeout(resolve, ms);
-  });
-}
-
-function secondaryFunction() {
-  // some ajax thing
-  console.log('ajax....')
-}
-
-
-funcName('Karen');
