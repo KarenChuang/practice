76863eeb81956c361201d8225fa6794f6d6e3ee5
KarenChuang <magic_gua@163.com>
2017-07-26 22:11:46 +0800

    es6:Set

--- learn-ES6/07-map-set.js
+++ learn-ES6/07-map-set.js
@@ -27,3 +27,20 @@ console.log('girl.keys', girl.keys()) // MapIterator
 for(key of girl.entries()) {
   console.log(key)
 }
+
+// 04
+const shopItems = ['Milk', 'Bread', 'Cheese', 'Chips', 'Milk']
+uniqueShopItems = new Set(shopItems)
+console.log(uniqueShopItems.values()) // SetIterator {"Milk", "Bread", "Cheese", "Chips"}
+
+uniqueShopItems.forEach((value) => {
+  console.log(value)
+})
+
+for(let value of uniqueShopItems.values()) {
+  console.log(value)
+}
+
+let list = [1,1,1,2,3,4,4,5]
+
+console.log([...new Set(list)]) // [1, 2, 3, 4, 5]
